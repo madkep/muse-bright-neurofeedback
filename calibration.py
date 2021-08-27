7b72a164758ad11b010e1f236642e241d7de2d6e
felipe cespedes <felipecespedes97@gmail.com>
2021-08-27 19:34:34 -0400

    rerework now the calibration are separated from the main program

--- calibration.py
+++ calibration.py
@@ -14,19 +14,57 @@ Adapted from https://github.com/NeuroTechX/bci-workshop
 """
 
 import numpy as np  # Module that simplifies computations on matrices
-import matplotlib.pyplot as plt  # Module used for plotting
+import matplotlib.pyplot as plt
+from numpy.random.mtrand import beta  # Module used for plotting
 from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
 import utils  # Our own utility functions
+from sys import exit
 
 # Handy little enum to make code more readable
 
+f = open('param.csv','r')
+message = f.read()
+
+theta_threshold = (float(message[0:4]))
+alpha_threshold = (float(message[5:9])) #SMR 
+beta_threshold = (float(message[10:14]))
+f.close()
+
 
 class Band:
-    Delta = 0
-    Theta = 1
-    Alpha = 2
-    Beta = 3
+    Delta_AF7 = 0
+    Delta_AF8 = 1
+    Theta_AF7 = 2
+    Theta_AF8 = 3
+    Alpha_AF7 = 4
+    Alpha_AF8 = 5
+    Beta_AF7 = 6
+    Beta_AF8 = 7
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+cal = False
+count = 0
+count_sec = 0
+calibration_time = 64.2 #4.2 seconds more for delay at start
+
+
+theta_cal = np.array([]) #Arrays for calibration and use
+alpha_cal= np.array([]) 
+beta_cal = np.array([])
+
+
+def seconds(sec):
+    return (sec*0.2)
 
 """ EXPERIMENTAL PARAMETERS """
 # Modify these to change aspects of the signal processing
@@ -46,8 +84,11 @@ SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
 
 # Index of the channel(s) (electrodes) to be used
 # 0 = left ear, 1 = left forehead, 2 = right forehead, 3 = right ear
-INDEX_CHANNEL = [1]
-INDEX_CHANNEL2 = [2]
+INDEX_CHANNEL = [1,2]
+ch_names = ['AF7', 'AF8']
+n_channels = 2
+
+feature_names = ['delta-AF7', 'delta-AF8', 'theta-AF7', 'theta-AF8', 'alpha-AF7', 'alpha-AF8','beta-AF7', 'beta-AF8']
 
 if __name__ == "__main__":
 
@@ -77,7 +118,7 @@ if __name__ == "__main__":
     """ 2. INITIALIZE BUFFERS """
 
     # Initialize raw EEG data buffer
-    eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
+    eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), n_channels))
     filter_state = None  # for use with the notch filter
 
     # Compute the number of epochs in "buffer_length"
@@ -86,7 +127,7 @@ if __name__ == "__main__":
 
     # Initialize the band power buffer (for plotting)
     # bands will be ordered: [delta, theta, alpha, beta]
-    band_buffer = np.zeros((n_win_test, 4))
+    band_buffer = np.zeros((n_win_test, len(feature_names)))
 
     """ 3. GET DATA """
 
@@ -97,69 +138,76 @@ if __name__ == "__main__":
     try:
         # The following loop acquires data, computes band powers, and calculates neurofeedback metrics based on those band powers
         while True:
-
+            count_sec += 1
             """ 3.1 ACQUIRE DATA """
             # Obtain EEG data from the LSL stream
             eeg_data, timestamp = inlet.pull_chunk(
                 timeout=1, max_samples=int(SHIFT_LENGTH * fs))
 
             # Only keep the channel we're interested in
-            ch_data1 = np.array(eeg_data)[:, INDEX_CHANNEL]
-            ch_data2 = np.array(eeg_data)[:, INDEX_CHANNEL2]
+            ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
 
             # Update EEG buffer with the new data
-            eeg_buffer1, filter_state = utils.update_buffer(
-                eeg_buffer, ch_data1, notch=True,
+            eeg_buffer, filter_state = utils.update_buffer(
+                eeg_buffer, ch_data, notch=True,
                 filter_state=filter_state)
-
-            eeg_buffer2, filter_state2 = utils.update_buffer(
-                eeg_buffer, ch_data2, notch=True,
-                filter_state=filter_state)
-
 
             """ 3.2 COMPUTE BAND POWERS """
             # Get newest samples from the buffer
-            data_epoch1 = utils.get_last_data(eeg_buffer1,
+            data_epoch = utils.get_last_data(eeg_buffer,
                                              EPOCH_LENGTH * fs)
 
-            data_epoch2 = utils.get_last_data(eeg_buffer2,
-                                    EPOCH_LENGTH * fs)
-                        
-
             # Compute band powers
-            band_powers1 = utils.compute_band_powers(data_epoch1, fs)
-            band_buffer1, _ = utils.update_buffer(band_buffer1,
-                                                 np.asarray([band_powers1]))
+            band_powers = utils.compute_band_powers(data_epoch, fs)
+            band_buffer, _ = utils.update_buffer(band_buffer,
+                                                 np.asarray([band_powers]))
             # Compute the average band powers for all epochs in buffer
             # This helps to smooth out noise
-            smooth_band_powers1 = np.mean(band_buffer1, axis=0)
-
-        
-            # Compute band powers
-            band_powers2 = utils.compute_band_powers(data_epoch2, fs)
-            band_buffer2, _ = utils.update_buffer(band_buffer2,
-                                                 np.asarray([band_powers2]))
-            # Compute the average band powers for all epochs in buffer
-            # This helps to smooth out noise
-            smooth_band_powers2 = np.mean(band_buffer2, axis=0)
+            #print(band_buffer)
+            smooth_band_powers = np.mean(band_buffer, axis=0)
 
             # print('Delta: ', band_powers[Band.Delta], ' Theta: ', band_powers[Band.Theta],
             #       ' Alpha: ', band_powers[Band.Alpha], ' Beta: ', band_powers[Band.Beta])
 
             """ 3.3 COMPUTE NEUROFEEDBACK METRICS """
-            # These metrics could also be used to drive brain-computer interfaces
+            
+            theta_percentage = (((smooth_band_powers[Band.Theta_AF7] + 2)*25) +  ((smooth_band_powers[Band.Theta_AF8] + 2)*25) )/2#theta represent theta
+            alpha_percentage = (((smooth_band_powers[Band.Alpha_AF7] + 2)*25) + ((smooth_band_powers[Band.Alpha_AF8] + 2)*25) )/2 #alpha represent SMR wave
+            beta_percentage = (((smooth_band_powers[Band.Beta_AF7] + 2)*25)  +  ((smooth_band_powers[Band.Beta_AF8] + 2)*25) )/2#beta represent high beta
 
-            # Alpha Protocol:
-            # Simple redout of alpha power, divided by delta waves in order to rule out noise
-            alpha_metric = smooth_band_powers1[Band.Alpha] / \
-                smooth_band_powers1[Band.Delta]
-            print('Alpha Relaxation: ', alpha_metric)
+            if(cal == False and count_sec  > 21):
+                if(seconds(count_sec) < calibration_time):
+                    theta_cal = np.append(theta_cal, theta_percentage)
+                    alpha_cal = np.append(alpha_cal, alpha_percentage)
+                    beta_cal = np.append(beta_cal, beta_percentage)
+                    
+                else:
+                    cal = True
+                    theta_average = np.mean(theta_cal)
+                    alpha_average = np.mean(alpha_cal)
+                    beta_average = np.mean(beta_cal)
+                    count_sec = 1
 
-            alpha_metric = smooth_band_powers2[Band.Alpha] / \
-                smooth_band_powers2[Band.Delta]
-            print('Alpha Relaxation: ', alpha_metric)
+                    option = input("Use " + str(alpha_average)[0:5] + " instead of " + str(alpha_threshold) +"? (y o n) higher better : " )
+                    if(option == "y"):
+                        alpha_threshold = alpha_average
 
-           
+                    option = input("Use " + str(theta_average)[0:5] + " instead of " + str(theta_threshold) +"? (y o n) lower better : " )
+                    if(option == "y"):
+                        theta_threshold = theta_average
+
+                    option = input("Use " + str(beta_average)[0:5] + " instead of " + str(beta_threshold) +"? (y o n) lower better : " )
+                    if(option == "y"):
+                        beta_threshold = beta_average
+
+                    f = open('param.csv','w')
+                    f.write(str(theta_threshold)[0:4] + "," + str(alpha_threshold)[0:4] + "," + str(beta_threshold)[0:4] )
+                    f.close()
+
+                    exit("Calibration successfully")
+                
+
+
 
     except KeyboardInterrupt:
         print('Closing!')
--- main.py
+++ main.py
@@ -1,56 +1,39 @@
 # -*- coding: utf-8 -*-
 """
 Estimate Relaxation from Band Powers
+
 This example shows how to buffer, epoch, and transform EEG data from a single
 electrode into values for each of the classic frequencies (e.g. alpha, beta, theta)
 Furthermore, it shows how ratios of the band powers can be used to estimate
 mental state for neurofeedback.
+
 The neurofeedback protocols described here are inspired by
 *Neurofeedback: A Comprehensive Review on System Design, Methodology and Clinical Applications* by Marzbani et. al
+
 Adapted from https://github.com/NeuroTechX/bci-workshop
 """
 
-from typing import Text
 import numpy as np  # Module that simplifies computations on matrices
-import matplotlib.pyplot as plt  # Module used for plotting
+import matplotlib.pyplot as plt
+from numpy.random.mtrand import beta  # Module used for plotting
 from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
 import utils  # Our own utility functions
 from os import system
-from time import sleep
 
 # Handy little enum to make code more readable
 
-#my dificult
-#level 1
-#theta < 60
-#smr(alpha) > 60
-#betha < 50
+
+count = 0
+count_sec = 0
+error_percentage = 2 # The calibration error
 
 f = open('param.csv','r')
 message = f.read()
 
-theta_threshold = (float(message[0:4]))
-alpha_threshold = (float(message[5:9])) #SMR 
-beta_threshold = (float(message[10:14]))
-
-theta_cal = np.array([])
-alpha_cal= np.array([])
-beta_cal = np.array([])
-
-theta_average = 0
-alpha_average = 0
-beta_average = 0
-
-calibration_time = 64.2 # add 4.2 sec to your calibration time
-error_percentage = 0 # The calibration error
-
+theta_threshold = (float(message[0:4])) + error_percentage
+alpha_threshold = (float(message[5:9])) - error_percentage#SMR 
+beta_threshold = (float(message[10:14])) + error_percentage
 f.close()
-
-sec_cal = 0
-sec_op = 0
-
-cal = False
-aux = np.array([])
 
 class Band:
     Delta_AF7 = 0
@@ -73,8 +56,12 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def seconds(sec):
-    return (sec*0.2)
+
+
+
+theta_cal = np.array([]) #Arrays for calibration and use
+alpha_cal= np.array([]) 
+beta_cal = np.array([])
 
 def put_10():
     system("brightness 0.01")
@@ -84,7 +71,6 @@ def put_50():
 
 def put_100():
     system("brightness 0.85")
-
 
 
 """ EXPERIMENTAL PARAMETERS """
@@ -106,18 +92,15 @@ SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
 # Index of the channel(s) (electrodes) to be used
 # 0 = left ear, 1 = left forehead, 2 = right forehead, 3 = right ear
 INDEX_CHANNEL = [1,2]
-
 ch_names = ['AF7', 'AF8']
 n_channels = 2
 
 feature_names = ['delta-AF7', 'delta-AF8', 'theta-AF7', 'theta-AF8', 'alpha-AF7', 'alpha-AF8','beta-AF7', 'beta-AF8']
 
-
 if __name__ == "__main__":
 
     """ 1. CONNECT TO EEG STREAM """
     put_100()
-
     # Search for active LSL streams
     print('Looking for an EEG stream...')
     streams = resolve_byprop('type', 'EEG', timeout=2)
@@ -158,23 +141,19 @@ if __name__ == "__main__":
     # The try/except structure allows to quit the while loop by aborting the
     # script with <Ctrl-C>
     print('Press Ctrl-C in the console to break the while loop.')
+
     try:
         # The following loop acquires data, computes band powers, and calculates neurofeedback metrics based on those band powers
         while True:
-
+            count += 1
+            count_sec += 1
             """ 3.1 ACQUIRE DATA """
             # Obtain EEG data from the LSL stream
             eeg_data, timestamp = inlet.pull_chunk(
                 timeout=1, max_samples=int(SHIFT_LENGTH * fs))
 
-            try:
-                # Only keep the channel we're interested in
-                aux = ch_data
-                ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
-
-            except:
-                # keep the last working state
-                ch_data = aux
+            # Only keep the channel we're interested in
+            ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
 
             # Update EEG buffer with the new data
             eeg_buffer, filter_state = utils.update_buffer(
@@ -192,109 +171,62 @@ if __name__ == "__main__":
                                                  np.asarray([band_powers]))
             # Compute the average band powers for all epochs in buffer
             # This helps to smooth out noise
+            #print(band_buffer)
             smooth_band_powers = np.mean(band_buffer, axis=0)
 
             # print('Delta: ', band_powers[Band.Delta], ' Theta: ', band_powers[Band.Theta],
             #       ' Alpha: ', band_powers[Band.Alpha], ' Beta: ', band_powers[Band.Beta])
 
             """ 3.3 COMPUTE NEUROFEEDBACK METRICS """
-            # These metrics could also be used to drive brain-computer interfaces
-            # Beta Protocol:
-            # Beta waves have been used as a measure of mental activity and concentration
-            # This beta over theta ratio is commonly used as neurofeedback for ADHD
             
             theta_percentage = (((smooth_band_powers[Band.Theta_AF7] + 2)*25) +  ((smooth_band_powers[Band.Theta_AF8] + 2)*25) )/2#theta represent theta
             alpha_percentage = (((smooth_band_powers[Band.Alpha_AF7] + 2)*25) + ((smooth_band_powers[Band.Alpha_AF8] + 2)*25) )/2 #alpha represent SMR wave
             beta_percentage = (((smooth_band_powers[Band.Beta_AF7] + 2)*25)  +  ((smooth_band_powers[Band.Beta_AF8] + 2)*25) )/2#beta represent high beta
-            print(str(smooth_band_powers[Band.Beta_AF7]) + "  " + str(smooth_band_powers[Band.Beta_AF8]) + " " + str(beta_percentage) + " " + str(beta_threshold))
 
-            if(sec_cal == 0):
-                option = input("Do you want to calibrate?(y o n) : ")
-                if( option == "y"):
-                    cal = False
-                elif(option == "n"):
-                    cal = True
-                    sec_cal = 1
-            sec_cal += 1
+            if(count % 6 == 0 and count_sec > 21):
+                theta_avg = np.mean(theta_cal)
+                alpha_avg = np.mean(alpha_cal)
+                beta_avg = np.mean(beta_cal)
+                
+                theta_cal = np.array([])
+                alpha_cal= np.array([]) 
+                beta_cal = np.array([])
+                count = 0
 
-            if(cal == False and sec_cal > 21):
-                if(seconds(sec_cal) < calibration_time):
-                    theta_cal = np.append(theta_cal, theta_percentage)
-                    alpha_cal = np.append(alpha_cal, alpha_percentage)
-                    beta_cal = np.append(beta_cal, beta_percentage)
-                    
-                else:
-                    cal = True
-                    theta_average = theta_cal.mean() + error_percentage
-                    alpha_average = alpha_cal.mean() - error_percentage
-                    beta_average = beta_cal.mean() + error_percentage
-                    sec_cal = 1
-
-                    option = input("Use " + str(alpha_average)[0:5] + " instead of " + str(alpha_threshold) +"? (y o n) higher better : " )
-                    if(option == "y"):
-                        alpha_threshold = alpha_average
-
-                    option = input("Use " + str(theta_average)[0:5] + " instead of " + str(theta_threshold) +"? (y o n) lower better : " )
-                    if(option == "y"):
-                        theta_threshold = theta_average
-
-                    option = input("Use " + str(beta_average)[0:5] + " instead of " + str(beta_threshold) +"? (y o n) lower better : " )
-                    if(option == "y"):
-                        beta_threshold = beta_average
-
-                    #clean the arrays
-                    theta_cal = np.array([])
-                    alpha_cal= np.array([]) 
-                    beta_cal = np.array([])
-
-            elif(cal == True and sec_cal > 21):
-                sec_op += 1
-                if(sec_op % 6 == 0):
-                    print(" ")
-                    theta_avg = theta_cal.mean()
-                    alpha_avg = alpha_cal.mean()
-                    beta_avg = beta_cal.mean()
-
-                    if( alpha_avg > alpha_threshold and theta_avg < theta_threshold and beta_avg < beta_threshold):
-                        put_100()
-                            
-                    elif( alpha_avg > alpha_threshold and theta_avg < theta_threshold ):
-                        put_50()
+                if( alpha_avg > alpha_threshold and theta_avg < theta_threshold and beta_avg < beta_threshold):
+                    put_100()
                         
-                    else:
-                        put_10()
-
-                    print(beta_avg)
+                elif( alpha_avg > alpha_threshold and theta_avg < theta_threshold ):
+                    put_50()
                     
-                    if(theta_avg < theta_threshold):
-                        print( bcolors.OKGREEN +"theta: " + str(theta_threshold) + bcolors.OKGREEN, end=' ')
-                    else:
-                        print( bcolors.FAIL +"theta: " + str(theta_threshold) + bcolors.FAIL, end=' ')
-
-                    if(alpha_avg > alpha_threshold):
-                        print(bcolors.OKGREEN + "SMR" + str(alpha_threshold) + bcolors.OKGREEN, end=' ')
-                    else:
-                        print(bcolors.FAIL + "SMR" + str(alpha_threshold) + bcolors.FAIL, end=' ')
-
-                    if(beta_avg < beta_threshold):
-                        print(bcolors.OKGREEN + "beta" + str(beta_threshold) + bcolors.OKGREEN, end='\n')
-                    else:
-                        print(bcolors.FAIL + "beta" + str(beta_threshold) + bcolors.FAIL, end='\n')
-
-                    #Clean the arrays<
-                    
-                    theta_cal = np.array([])
-                    alpha_cal= np.array([]) 
-                    beta_cal = np.array([])
-                    sec_op = 0
                 else:
-                    theta_cal = np.append(theta_cal, theta_percentage)
-                    alpha_cal = np.append(alpha_cal, alpha_percentage)
-                    beta_cal = np.append(beta_cal, beta_percentage)
+                    put_10()
 
-                    
+                print("    " + str(theta_avg)[0:5] + "    " + str(alpha_avg)[0:5]  + "    " + str(beta_avg)[0:5]  )
+
+                if(theta_avg < theta_threshold):
+                    print( bcolors.OKGREEN +"theta: " + str(theta_threshold) + bcolors.OKGREEN, end=' ')
+                else:
+                    print( bcolors.FAIL +"theta: " + str(theta_threshold) + bcolors.FAIL, end=' ')
+
+                if(alpha_avg > alpha_threshold):
+                    print(bcolors.OKGREEN + "SMR" + str(alpha_threshold) + bcolors.OKGREEN, end=' ')
+                else:
+                    print(bcolors.FAIL + "SMR" + str(alpha_threshold) + bcolors.FAIL, end=' ')
+
+                if(beta_avg < beta_threshold):
+                    print(bcolors.OKGREEN + "beta" + str(beta_threshold) + bcolors.OKGREEN, end='\n')
+                else:
+                    print(bcolors.FAIL + "beta" + str(beta_threshold) + bcolors.FAIL, end='\n')
+
+
+
+            elif(count_sec > 21):
+                theta_cal = np.append(theta_cal, theta_percentage)
+                alpha_cal = np.append(alpha_cal, alpha_percentage)
+                beta_cal = np.append(beta_cal, beta_percentage)
+
+
+
     except KeyboardInterrupt:
         print('Closing!')
-        f = open('param.csv','w')
-        f.write(str(theta_threshold)[0:4] + "," + str(alpha_threshold)[0:4] + "," + str(beta_threshold)[0:4] )
-        f.close()
